607e527efef4a89d1813757b7a1cb5da07280e34
David Kerr <david@kerrfamily.org>
2024-12-13 19:33:56 -0500

    Patch for WebServer.cpp to remove 'request handler not found' error message

---
+++ patch_files.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# This script runs patch command on specified files.
+#
+# Copyright (c) 2023 David Kerr, https://github.com/dkerr64
+#
+import os
+
+home_dir = os.environ['HOME'];
+
+os.system("patch -N " + home_dir + "/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/WebServer.cpp url_not_found_log.patch")
